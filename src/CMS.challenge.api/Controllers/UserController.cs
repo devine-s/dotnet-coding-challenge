f987c81463550fbd8cd07c7b8c4ac8b07767806c
Scott Devine <smd@cds-systems.co.uk>
2021-12-10 14:56:14 +0000

    Add basic logic to create and get users

--- src/CMS.challenge.api/Controllers/UserController.cs
+++ src/CMS.challenge.api/Controllers/UserController.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CMS.challenge.data.Cache;
+using CMS.challenge.data.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
 
 namespace CMS.challenge.api.Controllers
 {
@@ -7,16 +13,34 @@ namespace CMS.challenge.api.Controllers
     [Produces("application/json")]
     public class UserController : Controller
     {
-        [HttpPost]
-        public IActionResult CreateUser()
+        private readonly ISimpleObjectCache<Guid, User> _simpleObjectCache;
+
+        public UserController(ISimpleObjectCache<Guid, User> SimpleObjectCache)
         {
+            _simpleObjectCache = SimpleObjectCache;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUserAsync()
+        {
+            var user = new User
+            {
+                DateOfBirth = DateTime.Now.AddYears(-10),
+                Email = "",
+                FirstName = "",
+                LastName = "",
+                Id = Guid.NewGuid()
+            };
+
+            await _simpleObjectCache.AddAsync(user.Id, user);
+
             return Ok();
         }
 
         [HttpGet]
-        public IActionResult GetUsers()
+        public async Task<IActionResult> GetUsersAsync()
         {
-            return Ok();
+            return Ok(await _simpleObjectCache.GetAllAsync());
         }
 
         [HttpGet]
